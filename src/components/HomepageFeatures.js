0ea45d9731a67c7f3dbba44e2c5848db7466cb3a
Matheus Leonel Balduino <matheusl.balduino@gmail.com>
2021-11-16 10:58:40 -0300

    Add support for repo badges in project list

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,44 +1,64 @@
 import React from 'react';
 import styles from './HomepageFeatures.module.css';
+import Link from '@docusaurus/Link';
 
 const CardList = [
   {
     title: 'go-log',
     description: 'Opinionated, modularized, structured and production-ready GO logging library',
     link: 'mathbalduino.com.br/go-log',
+    gocovToken: 'X6MM6EFXGT',
   },
   {
     title: 'go-codegen',
     description: <>Coming soon<br />...</>,
     link: undefined,
+    gocovToken: '',
   },
   {
     title: 'go-test',
     description: <>Coming soon<br />...</>,
     link: undefined,
+    gocovToken: '',
   },
   {
     title: 'go-error',
     description: <>Coming soon<br />...</>,
     link: undefined,
+    gocovToken: '',
   },
   {
     title: 'go-validation',
     description: <>Coming soon<br />...</>,
     link: undefined,
+    gocovToken: '',
   },
   {
     title: 'go-migration',
     description: <>Coming soon<br />...</>,
     link: undefined,
+    gocovToken: '',
   },
 ];
 
-function Card({link, title, description}) {
+function Card({link, title, description, gocovToken}) {
   return (
     <div className="col col--4 margin-bottom--lg">
       <div className="card">
-        <div className="card__header"><h3>{title}</h3></div>
+        <div className="card__header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <h3>{title}</h3>
+          {!!gocovToken && (
+            <div>
+              <Link to={`https://github.com/mathbalduino/${title}/actions/workflows/go.yml`}>
+                <img src={`https://github.com/mathbalduino/${title}/actions/workflows/go.yml/badge.svg?branch=main`} />
+              </Link>
+              &nbsp; &nbsp;
+              <Link to={`https://codecov.io/gh/mathbalduino/${title}`}>
+                <img src={`https://codecov.io/gh/mathbalduino/${title}/branch/main/graph/badge.svg?token=${gocovToken}`}/>
+              </Link>
+            </div>
+          )}
+        </div>
         <div className="card__body"><p>{description}</p></div>
         <div className="card__footer">
           <a className={`button button--secondary button--block ${link ? '' : 'disabled'}`} href={link ? 'https://' + link : '#'}>
